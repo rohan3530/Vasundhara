607d06473a5b5c964c6e015f7826ccd031e002a4
ASUS <ASUS@LAPTOP-J9GPTN5H>
2023-05-05 16:09:17 +0530

    Machine Test

---
+++ UserPass/src/main/java/Data/LoginDao.java
@@ -0,0 +1,54 @@
+package Data;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import userModel.User;
+
+public class LoginDao {
+
+	public boolean validate(User user) throws ClassNotFoundException {
+		boolean status = false;
+
+		Class.forName("com.mysql.jdbc.Driver");
+
+		try (Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/mysql_database?useSSL=false", "root", "root");
+
+
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("select * from login where username = ? and password = ? ")) {
+			preparedStatement.setString(1, user.getUsername());
+			preparedStatement.setString(2, user.getPassword());
+
+			System.out.println(preparedStatement);
+			ResultSet rs = preparedStatement.executeQuery();
+			status = rs.next();
+
+		} catch (SQLException e) {
+
+			printSQLException(e);
+		}
+		return status;
+	}
+
+	private void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
+}
